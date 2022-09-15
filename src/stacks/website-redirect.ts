98aa24fcaef4efcac830122b9ddad7b6116faa35
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-16 00:44:28 +0100

    Updated imports

--- src/stacks/website-redirect.ts
+++ src/stacks/website-redirect.ts
@@ -1,12 +1,12 @@
 import { Construct } from "constructs";
 import { TerraformStack } from "cdktf";
-import { buildS3Backend } from "../lib/backends";
-import { buildAWSProvider } from "../lib/providers";
-import { createHostedZoneRecord, getHostedZone } from "../lib/route53";
-import { getHostedZoneCertificate } from "../lib/acm";
-import { buildRedirectCloudfrontDistribution } from "../lib/cloudfront";
-import { buildS3Bucket, setS3BucketBlockPublicAccess, setS3BucketWebsiteConfig } from "../lib/s3";
-import { AWS_ADMINISTRATIVE_REGION } from "../config";
+import { buildS3Backend } from "@/lib/backends";
+import { buildAWSProvider } from "@/lib/providers";
+import { createHostedZoneRecord, getHostedZone } from "@/lib/route53";
+import { getHostedZoneCertificate } from "@/lib/acm";
+import { buildRedirectCloudfrontDistribution } from "@/lib/cloudfront";
+import { buildS3Bucket, setS3BucketBlockPublicAccess, setS3BucketWebsiteConfig } from "@/lib/s3";
+import { AWS_ADMINISTRATIVE_REGION } from "@/config";
 
 export class WebsiteRedirectStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
