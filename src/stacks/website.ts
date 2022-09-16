73cd79cd5763b9ae7ebbecb38f67fa36d67df965
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-16 01:04:33 +0100

    Updated README and imports (#5)

--- src/stacks/website.ts
+++ src/stacks/website.ts
@@ -1,12 +1,12 @@
 import { Construct } from "constructs";
 import { TerraformStack } from "cdktf";
-import { buildS3Backend } from "../lib/backends";
-import { buildAWSProvider } from "../lib/providers";
-import { createHostedZoneRecord, getHostedZone } from "../lib/route53";
-import { getHostedZoneCertificate } from "../lib/acm";
-import { buildWebsiteCloudfrontDistribution, buildCloudfrontOAI } from "../lib/cloudfront";
-import { buildS3Bucket, setS3BucketPolicy, setS3BucketBlockPublicAccess } from "../lib/s3";
-import { AWS_ADMINISTRATIVE_REGION } from "../config";
+import { buildS3Backend } from "@/lib/backends";
+import { buildAWSProvider } from "@/lib/providers";
+import { createHostedZoneRecord, getHostedZone } from "@/lib/route53";
+import { getHostedZoneCertificate } from "@/lib/acm";
+import { buildWebsiteCloudfrontDistribution, buildCloudfrontOAI } from "@/lib/cloudfront";
+import { buildS3Bucket, setS3BucketPolicy, setS3BucketBlockPublicAccess } from "@/lib/s3";
+import { AWS_ADMINISTRATIVE_REGION } from "@/config";
 
 export class WebsiteStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
